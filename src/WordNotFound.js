82052701d60e6651beb55522b333cb460109cfb3
christinesn <51234471+christinesn@users.noreply.github.com>
2024-01-02 13:32:40 -0500

    Style WordNotFound component

--- src/WordNotFound.js
+++ src/WordNotFound.js
@@ -1,18 +1,33 @@
 import './WordNotFound.css';
 
-function WordNotFound ({ data, handleSearch }) {
+function WordNotFound ({ data, handleSearch, searched, offensive }) {
+    if (offensive) {
+        return (
+            <div className="not-found">
+                <div className="not-found-prompt">
+                    Sorry, we couldn't find that word.
+                    <p>Try searching something else.</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            We couldn't find that word. Did you mean one of these?
-            <div>
+        <div className="not-found">
+            <div className="not-found-prompt">
+                Sorry, we couldn't find <span className="searched">{searched}</span>.
+                <p>Did you mean one of these words?</p>
+            </div>
+            <div className="suggestions nyms-section">
                 {data.map((word, i) => (
-                    <button
-                        className="suggestion"
-                        key={i}
-                        onClick={() => handleSearch(word)}
-                    >
-                        {word}
-                    </button>
+                    <div className="nym-container" key={i}>
+                        <button
+                            className="suggestion nym"
+                            onClick={() => handleSearch(word)}
+                        >
+                            {word}
+                        </button>
+                    </div>
                 ))}
             </div>
         </div>
