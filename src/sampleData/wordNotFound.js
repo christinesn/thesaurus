f3ff78d47e3f538bd7393fbea4242ad715e42d05
christinesn <51234471+christinesn@users.noreply.github.com>
2024-01-02 13:32:53 -0500

    Add word not found sample data

---
+++ src/sampleData/wordNotFound.js
@@ -0,0 +1,24 @@
+const wordNotFound = [
+    "hush",
+    "hushed",
+    "gush",
+    "shushed",
+    "husked",
+    "hushes",
+    "bush",
+    "cushy",
+    "gushy",
+    "hued",
+    "humid",
+    "husk",
+    "husks",
+    "husky",
+    "hussy",
+    "lush",
+    "mush",
+    "mushy",
+    "push",
+    "pushy"
+];
+
+export default wordNotFound
